e90f3db351948aaec3532ca4ae3d16bb1243969c
fcojj <fco.jonas.rodrigues@gmail.com>
2025-09-28 20:09:29 +0100

    Bootstrap quarkus rest api project

---
+++ src/test/java/tech/ada/web2/quarkus/BancosIT.java
@@ -0,0 +1,8 @@
+package tech.ada.web2.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class BancosIT extends BancosTest {
+    // Execute the same tests but in packaged mode.
+}
